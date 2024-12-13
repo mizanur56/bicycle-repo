3b29730bcf94a5762b7d09ee8877d19d3efb76ea
mizanur56 <mizan1.rahman10@gmail.com>
2024-12-13 09:53:28 +0600

    first commit

---
+++ bicycle.js
@@ -0,0 +1,18 @@
+const searchIcon = document.getElementById("search");
+const searchField = document.getElementById("input");
+
+searchIcon.onclick = () => {
+  searchField.classList.toggle("hidden");
+};
+
+const slides = document.querySelectorAll(".carousel-item");
+let currentIndex = 0;
+
+function showNextSlide() {
+  slides[currentIndex].classList.add("hidden");
+  currentIndex = (currentIndex + 1) % slides.length;
+  slides[currentIndex].classList.remove("hidden");
+}
+
+// Automatically move slides every 3 seconds
+setInterval(showNextSlide, 3000);
